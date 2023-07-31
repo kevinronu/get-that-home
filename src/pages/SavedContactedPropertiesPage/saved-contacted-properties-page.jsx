5791ad6fda19052612973f134e2c21350f884401
Kevin Robles <kevinronu@gmail.com>
2023-07-31 13:15:22 -0500

    feat: add saved contacted properties page

--- src/pages/SavedContactedPropertiesPage/saved-contacted-properties-page.jsx
+++ src/pages/SavedContactedPropertiesPage/saved-contacted-properties-page.jsx
@@ -1,3 +1,13 @@
+import Footer from "../../components/Footer";
+import HeaderSeeker from "../../components/HeaderSeeker";
+import SavedContactedPropertiesSection from "../../components/SavedConcactedPropertiesSection/saved-contacted-properties-section";
+
 export default function SavedContactedPropertiesPage() {
-  return <>SavedContactedPropertiesPage</>;
+  return (
+    <>
+      <HeaderSeeker />
+      <SavedContactedPropertiesSection />
+      <Footer />
+    </>
+  );
 }

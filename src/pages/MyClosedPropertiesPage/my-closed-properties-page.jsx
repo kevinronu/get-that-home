a886bc42118829fb4fcdbab822b4fa458cf860a0
Kevin Robles <kevinronu@gmail.com>
2023-07-31 14:26:27 -0500

    feat: add closed properties page

--- src/pages/MyClosedPropertiesPage/my-closed-properties-page.jsx
+++ src/pages/MyClosedPropertiesPage/my-closed-properties-page.jsx
@@ -1,3 +1,16 @@
+import HeaderLandlord from "../../components/HeaderLandlord";
+import Footer from "../../components/Footer";
+import Container from "../../layout/Container";
+import MyClosedPropertiesSection from "../../components/MyClosedPropertiesSection";
+
 export default function MyClosedPropertiesPage() {
-  return <>MyClosedPropertiesPage</>;
+  return (
+    <>
+      <HeaderLandlord />
+      <Container size="xl">
+        <MyClosedPropertiesSection />
+      </Container>
+      <Footer />
+    </>
+  );
 }

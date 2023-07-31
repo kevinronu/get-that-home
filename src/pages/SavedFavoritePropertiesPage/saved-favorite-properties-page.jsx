5115797eefa2573a25d5e2d3c9a787022e330296
Kevin Robles <kevinronu@gmail.com>
2023-07-31 13:15:29 -0500

    feat: add saved favorite properties page

--- src/pages/SavedFavoritePropertiesPage/saved-favorite-properties-page.jsx
+++ src/pages/SavedFavoritePropertiesPage/saved-favorite-properties-page.jsx
@@ -1,3 +1,13 @@
+import Footer from "../../components/Footer";
+import HeaderSeeker from "../../components/HeaderSeeker";
+import SavedFavoritePropertiesSection from "../../components/SavedFavoritePropertiesSection/saved-favorite-properties-section";
+
 export default function SavedFavoritePropertiesPage() {
-  return <>SavedFavoritePropertiesPage</>;
+  return (
+    <>
+      <HeaderSeeker />
+      <SavedFavoritePropertiesSection />
+      <Footer />
+    </>
+  );
 }

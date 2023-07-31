0aeec81fb5cf8bce3111d67e619c8ec80a16ef6f
Kevin Robles <kevinronu@gmail.com>
2023-07-31 17:01:24 -0500

    feat: add properties-page

--- src/pages/PropertiesPage/properties-page.jsx
+++ src/pages/PropertiesPage/properties-page.jsx
@@ -1,3 +1,25 @@
+import { useContext } from "react";
+
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import VisitorPropertiesSection from "../../components/VisitorPropertiesSection";
+import { AuthContext } from "../../context/auth-context";
+import LandlordPropertiesSection from "../../components/LandlordPropertiesSection";
+import SeekerPropertiesSection from "../../components/SeekerPropertiesSection";
+
 export default function PropertiesPage() {
-  return <>PropertiesPage</>;
+  const { user } = useContext(AuthContext);
+  return (
+    <>
+      <Header user={user} />
+      {!user ? (
+        <VisitorPropertiesSection />
+      ) : user.role === "landlord" ? (
+        <LandlordPropertiesSection />
+      ) : (
+        <SeekerPropertiesSection />
+      )}
+      <Footer />
+    </>
+  );
 }

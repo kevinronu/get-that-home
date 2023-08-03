d5c3f25c0546e34635f90f58e2a48b3d9e9ce254
Kevin Robles <kevinronu@gmail.com>
2023-08-03 15:09:19 -0500

    chore: add filter price component

---
+++ src/components/FilterPrice/filter-price.jsx
@@ -0,0 +1,94 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { BiSolidDollarCircle } from "react-icons/bi";
+import { BsDashLg } from "react-icons/bs";
+
+import Button from "../Button";
+import InputWithIcon from "../InputWithIcon";
+import { StyledContainer } from "./styles";
+
+export default function FilterPrice({ handleFilters }) {
+  const [showFilter, setShowFilter] = useState(false);
+
+  const [formData, setFormData] = useState({
+    minPrice: 0,
+    maxPrice: Infinity,
+  });
+
+  const { minPrice, maxPrice } = formData;
+
+  const handleToggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    const newValues = { ...formData, [name]: value };
+    setFormData(newValues);
+  }
+
+  function calcText() {
+    if (minPrice === 0 && maxPrice === Infinity) {
+      return "PRICE";
+    } else if (minPrice != 0 && maxPrice === Infinity) {
+      return `>= $${minPrice}k`;
+    } else if (minPrice === 0 && maxPrice != Infinity) {
+      return `<= $${maxPrice}k`;
+    } else {
+      return `$${minPrice}k - $${maxPrice}k`;
+    }
+  }
+
+  return (
+    <StyledContainer>
+      <div className="filter-price-button">
+        <Button type="primary" onClick={handleToggleFilter}>
+          {calcText()}
+        </Button>
+      </div>
+      {showFilter && (
+        <div className="container">
+          <form className="form">
+            <div>
+              <p className="form__label">PRICE RANGE</p>
+              <div className="form__inputs">
+                <InputWithIcon
+                  icon={<BiSolidDollarCircle size={"2rem"} />}
+                  name="minPrice"
+                  type="number"
+                  placeholder="min"
+                  onChange={handleChange}
+                  value={minPrice}
+                />
+                <BsDashLg size={"0.6875rem"} />
+                <InputWithIcon
+                  icon={<BiSolidDollarCircle size={"2rem"} />}
+                  name="maxPrice"
+                  type="number"
+                  placeholder="max"
+                  onChange={handleChange}
+                  value={maxPrice}
+                />
+              </div>
+            </div>
+            <Button
+              type="primary"
+              className="form__button"
+              onClick={() => handleFilters(formData)}
+            >
+              DONE
+            </Button>
+          </form>
+        </div>
+      )}
+    </StyledContainer>
+  );
+}
+
+FilterPrice.propTypes = {
+  handleFilters: PropTypes.func,
+};
---
+++ src/components/FilterPrice/styles.js
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import { fonts } from "../../styles";
+
+export const StyledContainer = styled.div`
+  position: relative;
+
+  .filter-price-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .container {
+    width: 15.5rem;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    background-color: ${(props) => props.theme.colors.background.lighter};
+    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2);
+    position: absolute;
+    // top: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+
+  .form__label {
+    font-family: ${fonts.secondary};
+    font-size: 0.625rem;
+    letter-spacing: 0.09375rem;
+    margin-bottom: 0.25rem;
+  }
+
+  .form__inputs {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .form__button {
+    place-self: flex-end;
+  }
+`;

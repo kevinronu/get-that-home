1a489dc45bba9a03108c84ff12709aa0460a6d96
Kevin Robles <kevinronu@gmail.com>
2023-08-03 15:57:51 -0500

    chore: add utils functions

---
+++ src/utils/utils.js
@@ -0,0 +1,28 @@
+export function filterProperties(
+  properties,
+  filters = {
+    address: "",
+    minPrice: 0,
+    maxPrice: Infinity,
+    propertyType: "all",
+    minBeds: 0,
+    minBaths: 0,
+    petsAllowed: "all",
+    minArea: 0,
+    maxArea: Infinity,
+  }
+) {
+  return properties.filter((property) => {
+    if (property.operation_type === "rent") {
+      return (
+        property.monthly_rent > filters.minPrice * 1000 &&
+        property.monthly_rent < filters.maxPrice * 1000
+      );
+    } else if (property.operation_type === "sale") {
+      return (
+        property.price * 1000 > filters.minPrice &&
+        property.price < filters.maxPrice * 1000
+      );
+    }
+  });
+}

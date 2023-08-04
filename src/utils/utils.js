233ced8834ff36b6d146441aa2583453be33f34b
Kevin Robles <kevinronu@gmail.com>
2023-08-03 21:26:10 -0500

    chore: update filters

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,7 @@ export function filterProperties(
     address: "",
     minPrice: 0,
     maxPrice: Infinity,
-    propertyType: "all",
+    propertyType: [],
     minBeds: 0,
     minBaths: 0,
     petsAllowed: "all",
@@ -12,17 +12,23 @@ export function filterProperties(
     maxArea: Infinity,
   }
 ) {
-  return properties.filter((property) => {
-    if (property.operation_type === "rent") {
-      return (
-        property.monthly_rent > filters.minPrice * 1000 &&
-        property.monthly_rent < filters.maxPrice * 1000
-      );
-    } else if (property.operation_type === "sale") {
-      return (
-        property.price * 1000 > filters.minPrice &&
-        property.price < filters.maxPrice * 1000
-      );
-    }
-  });
+  return properties
+    .filter((property) => {
+      if (property.operation_type === "rent") {
+        return (
+          property.monthly_rent > filters.minPrice * 1000 &&
+          property.monthly_rent < filters.maxPrice * 1000
+        );
+      } else if (property.operation_type === "sale") {
+        return (
+          property.price * 1000 > filters.minPrice &&
+          property.price < filters.maxPrice * 1000
+        );
+      }
+    })
+    .filter(
+      (property) =>
+        filters.propertyType.length === 0 ||
+        filters.propertyType.includes(property.property_type)
+    );
 }

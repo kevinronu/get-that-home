6f067c76f5aaf7d009de5e6d826d84b0340dc0eb
Kevin Robles <kevinronu@gmail.com>
2023-08-04 15:03:23 -0500

    chore: add filters to visitor properties section

--- src/components/VisitorPropertiesSection/visitor-properties-section.jsx
+++ src/components/VisitorPropertiesSection/visitor-properties-section.jsx
@@ -1,33 +1,34 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { PropertyContext } from "../../context/property-context";
+import FilterPropertiesPage from "../FiltersPropertiesPage";
 import { PropertiesContainer, StyledH2 } from "./styles";
-import { AuthContext } from "../../context/auth-context";
 import VisitorPropertyCard from "../VisitorPropertyCard";
-import Pagination from "../Pagination";
 import Container from "../../layout/Container";
 import Section from "../../layout/Section";
+import Pagination from "../Pagination";
 
 export default function VisitorPropertiesSection() {
   const { page } = useParams();
-  const { properties } = useContext(AuthContext);
+  const { filteredProperties } = useContext(PropertyContext);
   const [quantityPerPage] = useState(12);
   const [totalQuantity, setTotalQuantity] = useState(0);
   const lastIndex = page * quantityPerPage;
   const firstIndex = lastIndex - quantityPerPage;
 
   useEffect(() => {
-    setTotalQuantity(properties.length);
-  }, [properties]);
+    setTotalQuantity(filteredProperties.length);
+  }, [filteredProperties]);
 
   return (
     <Section size="xs">
       <Container size="xl" padding={"1rem"}>
         <>
-          <div>Filters</div>
+          <FilterPropertiesPage />
           <StyledH2>{totalQuantity} Properties found</StyledH2>
           <PropertiesContainer>
-            {properties.slice(firstIndex, lastIndex).map((property) => {
+            {filteredProperties.slice(firstIndex, lastIndex).map((property) => {
               return (
                 <VisitorPropertyCard
                   key={`property-${property.id}`}

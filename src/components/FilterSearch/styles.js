62159542e9b370a82bac4821fde684d8e94e2678
Kevin Robles <kevinronu@gmail.com>
2023-08-04 14:56:56 -0500

    chore: update styles

--- src/components/FilterSearch/styles.js
+++ src/components/FilterSearch/styles.js
@@ -8,4 +8,16 @@ export const StyledContainer = styled.div`
   border-radius: 0.5rem;
   border: 0.0625rem solid ${(props) => props.theme.colors.pink[500]};
   background-color: ${(props) => props.theme.colors.background.lighter};
+
+  .input {
+    font-family: ${fonts.secondary};
+  }
+
+  .input::placeholder {
+    color: ${(props) => props.theme.colors.text.light};
+  }
+
+  .input:focus {
+    outline: 0.1875rem solid ${(props) => props.theme.colors.info};
+  }
 `;

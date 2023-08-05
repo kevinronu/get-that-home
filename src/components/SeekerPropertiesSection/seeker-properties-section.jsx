f536423dbee2415541134eb405b59825d80278b6
Kevin Robles <kevinronu@gmail.com>
2023-08-04 22:53:43 -0500

    chore: add filters

--- src/components/SeekerPropertiesSection/seeker-properties-section.jsx
+++ src/components/SeekerPropertiesSection/seeker-properties-section.jsx
@@ -8,10 +8,11 @@ import SeekerPropertyCard from "../SeekerPropertyCard";
 import Pagination from "../Pagination";
 import { SeekerContext } from "../../context/seeker-context";
 import { PropertyContext } from "../../context/property-context";
+import FilterPropertiesPage from "../FiltersPropertiesPage";
 
 export default function SeekerPropertiesSection() {
   const { page } = useParams();
-  const { properties } = useContext(PropertyContext);
+  const { filteredProperties } = useContext(PropertyContext);
   const { favorites } = useContext(SeekerContext);
   const [quantityPerPage] = useState(12);
   const [totalQuantity, setTotalQuantity] = useState(0);
@@ -19,17 +20,17 @@ export default function SeekerPropertiesSection() {
   const firstIndex = lastIndex - quantityPerPage;
 
   useEffect(() => {
-    setTotalQuantity(properties.length);
-  }, [properties]);
+    setTotalQuantity(filteredProperties.length);
+  }, [filteredProperties]);
 
   return (
     <Section size="xs">
       <Container size="xl" padding={"1rem"}>
         <>
-          <div>Filters</div>
+          <FilterPropertiesPage />
           <StyledH2>{totalQuantity} Properties found</StyledH2>
           <PropertiesContainer>
-            {properties.slice(firstIndex, lastIndex).map((property) => {
+            {filteredProperties.slice(firstIndex, lastIndex).map((property) => {
               const isFavorite = favorites.some(
                 (favorite) => favorite.id === property.id
               );

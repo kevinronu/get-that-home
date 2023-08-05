3180d327947a2fcef00b9a3f74f0209f8fef61fd
Kevin Robles <kevinronu@gmail.com>
2023-08-05 16:52:46 -0500

    feat: add property detail page

--- src/pages/PropertyDetailPage/property-detail-page.jsx
+++ src/pages/PropertyDetailPage/property-detail-page.jsx
@@ -1,5 +1,18 @@
-function PropertyDetailPage() {
-  return <>PropertyDetailPage</>;
-}
+import { useContext } from "react";
 
-export default PropertyDetailPage;
+import PropertyDetailSection from "../../components/PropertyDetailSection";
+import { AuthContext } from "../../context/auth-context";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function PropertyDetailPage() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <>
+      <Header user={user} />
+      <PropertyDetailSection />
+      <Footer />
+    </>
+  );
+}

88c3d52200205085846e7802dcd742c443f7bc9d
Kevin Robles <kevinronu@gmail.com>
2023-08-06 13:12:43 -0500

    chore: fix warning

--- src/components/SignUpSection/sign-up-section.jsx
+++ src/components/SignUpSection/sign-up-section.jsx
@@ -9,24 +9,28 @@ export default function SignUpSection() {
   return (
     <StyledSection>
       <Container size={"xl"} padding={"1rem"}>
-        <div className="header">
-          <p className="header__instructions">
-            Select the profile with which you identify
-          </p>
-          <h1 className="header__title">What are you looking for?</h1>
-        </div>
-        <div className="options">
-          <Link to={"landlord"} className="link">
-            <img src={landlord} alt="landlord" className="link__img" />
-            <h2 className="link__title">Landlord</h2>
-            <p className="link__description">You want to rent or sell a home</p>
-          </Link>
-          <Link to={"seeker"} className="link">
-            <img src={seeker} alt="seeker" className="link__img" />
-            <h2 className="link__title">Home seeker</h2>
-            <p className="link__description">You want to find a home</p>
-          </Link>
-        </div>
+        <>
+          <div className="header">
+            <p className="header__instructions">
+              Select the profile with which you identify
+            </p>
+            <h1 className="header__title">What are you looking for?</h1>
+          </div>
+          <div className="options">
+            <Link to={"landlord"} className="link">
+              <img src={landlord} alt="landlord" className="link__img" />
+              <h2 className="link__title">Landlord</h2>
+              <p className="link__description">
+                You want to rent or sell a home
+              </p>
+            </Link>
+            <Link to={"seeker"} className="link">
+              <img src={seeker} alt="seeker" className="link__img" />
+              <h2 className="link__title">Home seeker</h2>
+              <p className="link__description">You want to find a home</p>
+            </Link>
+          </div>
+        </>
       </Container>
     </StyledSection>
   );

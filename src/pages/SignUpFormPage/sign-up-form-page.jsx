20110001365caa8583ab108d155cc6d2bace34ec
Kevin Robles <kevinronu@gmail.com>
2023-08-06 15:18:53 -0500

    feat: add sign up form page

--- src/pages/SignUpFormPage/sign-up-form-page.jsx
+++ src/pages/SignUpFormPage/sign-up-form-page.jsx
@@ -1,3 +1,13 @@
+import Footer from "../../components/Footer";
+import HeaderVisitor from "../../components/HeaderVisitor";
+import SignUpFormSection from "../../components/SignUpFormSection";
+
 export default function SignUpFormPage() {
-  return <>SignUpFormPage</>;
+  return (
+    <>
+      <HeaderVisitor />
+      <SignUpFormSection />
+      <Footer />
+    </>
+  );
 }

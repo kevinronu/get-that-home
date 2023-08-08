34830d959f824a85ff9623c230f08ecef7be1f47
Kevin Robles <kevinronu@gmail.com>
2023-08-08 16:58:52 -0500

    feat: add edit sale property page

--- src/pages/EditSalePropertyPage/edit-sale-property-page.jsx
+++ src/pages/EditSalePropertyPage/edit-sale-property-page.jsx
@@ -1,5 +1,15 @@
+import EditSalePropertySection from "../../components/EditSalePropertySection";
+import HeaderLandlord from "../../components/HeaderLandlord";
+import Footer from "../../components/Footer";
+
 function EditSalePropertyPage() {
-  return <>EditSalePropertyPage</>;
+  return (
+    <>
+      <HeaderLandlord />
+      <EditSalePropertySection />
+      <Footer />
+    </>
+  );
 }
 
 export default EditSalePropertyPage;

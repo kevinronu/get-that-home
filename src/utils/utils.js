b8c0c3381bfc93aa58a19cdc7bbad1de63356e6e
Kevin Robles <kevinronu@gmail.com>
2023-08-10 18:22:40 -0500

    chore: refactor filter function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import { ALL_FILTERS_SELECTED } from "../const/filters";
+
 export function filterProperties(
   properties,
   filters = {
@@ -7,58 +9,63 @@ export function filterProperties(
     propertyType: [],
     minBeds: 0,
     minBaths: 0,
-    petsAllowed: false,
+    petsAllowed: null,
     minArea: 0,
     maxArea: Infinity,
-    operationType: "all",
+    operationType: ALL_FILTERS_SELECTED,
   }
 ) {
-  return properties
-    .filter((property) => !property.closed)
-    .filter((property) => {
-      const regexAddress = new RegExp(filters.address, "i");
-      const regexCity = new RegExp(filters.city, "i");
-      return (
-        filters.address === "" ||
-        property.address.search(regexAddress) >= 0 ||
-        property.address.search(regexCity) >= 0
-      );
-    })
-    .filter((property) => {
-      if (property.operation_type === "rent") {
-        return (
-          property.monthly_rent >= filters.minPrice * 1000 &&
-          property.monthly_rent <= filters.maxPrice * 1000
-        );
-      } else if (property.operation_type === "sale") {
-        return (
-          property.price * 1000 >= filters.minPrice &&
-          property.price <= filters.maxPrice * 1000
-        );
-      }
-    })
-    .filter(
-      (property) =>
-        filters.propertyType.length === 0 ||
-        filters.propertyType.includes(property.property_type)
+  return properties.filter((property) => {
+    if (property.closed) return false;
+
+    if (filters.address) {
+      if (
+        !property.address.toLowerCase().includes(filters.address.toLowerCase())
+      )
+        return false;
+    }
+
+    if (property.operation_type === "rent") {
+      if (
+        property.monthly_rent < filters.minPrice * 1000 ||
+        property.monthly_rent > filters.maxPrice * 1000
+      )
+        return false;
+    } else if (property.operation_type === "sale") {
+      if (
+        property.price * 1000 < filters.minPrice ||
+        property.price > filters.maxPrice * 1000
+      )
+        return false;
+    }
+
+    if (
+      filters.propertyType.length != 0 &&
+      !filters.propertyType.includes(property.property_type)
     )
-    .filter(
-      (property) =>
-        property.bedrooms >= filters.minBeds &&
-        property.bathrooms >= filters.minBaths
+      return false;
+
+    if (
+      property.bedrooms < filters.minBeds ||
+      property.bathrooms < filters.minBaths
     )
-    .filter(
-      (property) =>
-        (!filters.petsAllowed ||
-          property.pets_allowed >= filters.petsAllowed) &&
-        property.area >= filters.minArea &&
-        property.area <= filters.maxArea
+      return false;
+
+    if (filters.petsAllowed != null) {
+      if (property.pets_allowed != filters.petsAllowed) return false;
+    }
+
+    if (property.area < filters.minArea || property.area > filters.maxArea)
+      return false;
+
+    if (
+      filters.operationType != ALL_FILTERS_SELECTED &&
+      filters.operationType != property.operation_type
     )
-    .filter(
-      (property) =>
-        filters.operationType === "all" ||
-        filters.operationType === property.operation_type
-    );
+      return false;
+
+    return true;
+  });
 }
 
 export function getCoordinates(address) {
